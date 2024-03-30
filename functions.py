a47e68aed572fa7b545b764e44641c1e00385f77
GriselOrtizC <93092096+GriselOrtizClavijo@users.noreply.github.com>
2024-03-30 17:28:07 +0100

    fix convert_fahrenheit_to_celsius

--- functions.py
+++ functions.py
@@ -7,10 +7,5 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
-#def convert_fahrenheit_to_celsius(fahrenheit):
-#    return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
-
 def convert_fahrenheit_to_celsius(fahrenheit):
- if fahrenheit < -459.67:
-    raise ValueError("El valor de Fahrenheit no puede ser menor que -459.67")
-    return multiply(subtract(fahrenheit, 32), 5 / 9)
+    return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
