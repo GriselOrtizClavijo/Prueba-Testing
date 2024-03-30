bf041f7af32ee4ed033356379a221b5ff903f900
GriselOrtizC <93092096+GriselOrtizClavijo@users.noreply.github.com>
2024-03-30 17:47:05 +0100

    Assertion error

--- functions.py
+++ functions.py
@@ -10,5 +10,5 @@ def multiply(a, b):
 def convert_fahrenheit_to_celsius(fahrenheit):
     celsius = multiply(subtract(fahrenheit, 32), 5 / 9)
     if celsius < 0:
-            raise ValueError("El resultado es menor que 0 en Celsius")
+            raise AssertionError("El resultado es menor que 0 en Celsius")
     return celsius
