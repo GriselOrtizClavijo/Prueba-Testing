96ddd34a24fbf9cf434f672ae101ac3c2452bacb
GriselOrtizC <93092096+GriselOrtizClavijo@users.noreply.github.com>
2024-03-30 17:23:52 +0100

    Change function covert_farenheit

--- functions.py
+++ functions.py
@@ -7,5 +7,10 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+#def convert_fahrenheit_to_celsius(fahrenheit):
+#    return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
+
 def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
+ if fahrenheit < -459.67:
+    raise ValueError("El valor de Fahrenheit no puede ser menor que -459.67")
+    return multiply(subtract(fahrenheit, 32), 5 / 9)
